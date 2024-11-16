0b311ac9eeb3227ac1cbb0405a675ffba04624b6
halldev <halldev@halldev.home>
2024-11-16 07:31:11 -0500

    some notes and ideas for message parser

---
+++ app/kafka_consumer/consumer_parser/parser.py
@@ -0,0 +1,19 @@
+#consumer parser class will parse message and return data in parsed message
+'''
+Params to determine general message structure and format? 
+- - - These params will enable message parser to understand how to read the message
+    - attributes/attribute defintions
+    - delmiters?
+    - grammar?
+    - where will parser params come from?/how will they be passed?
+    - data types?
+
+Parser general methods/operations (draft)
+    - store attributes/message data
+    - convert attributes to specified type
+    - send data somehere (this is why it needs to be stored) to be used by application
+    - #TODO - - - Brainstorm more possible operations that parser may need.
+    
+
+
+'''

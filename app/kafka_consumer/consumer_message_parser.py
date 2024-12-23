fbdece3bf9fe957cac6913b52424a44b0ea57055
hallsamir14 <hallsamir14@gmail.com>
2024-12-22 23:34:53 -0500

    add some comments, work on method prototypes

--- app/kafka_consumer/consumer_message_parser.py
+++ app/kafka_consumer/consumer_message_parser.py
@@ -28,23 +28,25 @@ schema built from user parameters
     - - - First key will always be message type
 
 
+
 """
 
 
 # parser to be used by consumer message processor to read, validate message and store message data
 class parser:
 
-    # schema format is needed along with message format, how to implement logic?
-    def __init__(self, message_format: message_format):
-        if message_format == message_format.JSON:
-            self.schema: dict[str, str] = []
-
-    def validate_message(self):
+    '''
+    - parser constructor will use message type to pull associated data (schema,message format)
+    - at the point where the parser is intialized, message type mappings and user paramaters will be stored in application cache ...
+    - constructor will only need message type to be able to retrieve expected message schema and parser rules ...
+    '''
+    def __init__(self, message_type):
+        pass
+    
+    #private method that checks if message aligns with schema of corresponding message type
+    def __isvalid_message(self) -> bool:
         pass
 
-    # To be used in parse_message method, will
-    def store_message_data(self, attribute: Any) -> list[Any]:
-        pass
-
-    def parse_json_message(self) -> int:
+    #successfully parsed message will return dictionary of attribute names and their values
+    def parse (self) -> dict:
         pass
